d6afdee1ac13b6f823323ed6d6c4e857e810d6c9
Patrick-Santana <zonaabissal2016@gmail.com>
2020-01-31 22:17:29 -0300

    Add files via upload

---
+++ game/velhatest/src/velhatest/Velhatest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package velhatest;
+
+/**
+ *
+ * @author Patric
+ */
+public class Velhatest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Game FrameGame = new Game();
+        FrameGame.setVisible(true); //Método que chama o form jframe 
+    }
+    
+}
